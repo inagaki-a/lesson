b63356408c54f69789f305a4b3639db5ca0c7fcf
inagaki <hdj16802@gmail.com>
2021-05-02 07:44:56 +0900

    K303_01修正

--- k303_01.py
+++ k303_01.py
@@ -1,26 +1,54 @@
 class Pikachu():
 
     #初期化メソッド
-    def __init__(self, power, attack, defense, speed, technique1, technique2, technique3): #初期化
+    def __init__(self, name, power, attack, defense, speed): #初期化
+        self.name = name
         self.power = power
         self.attack = attack
         self.defense = defense 
-        self.speed = speed 
-        self.technique1 = technique1
-        self.technique2 = technique2
-        self.technique3 = technique3
+        self.speed = speed
+        self.waza = 1
     
     def status(self):
-        return self.power, self.attack, self.defense, self.speed, self.technique1, self.technique2, self.technique3, self.technique4, self.technique5, self.technique6,
+        return self.power, self.attack, self.defense, self.speed
+    
+    def denkou(self):
+        damage = self.power * self.waza * 1
+        print(self.name + "が電光石化をした。(ダメージ：" + str(damage) + ")")
+
+    def bolt(self):
+        damage = self.power * self.waza * 2
+        print(self.name + "が10万ボルトをした。(ダメージ：" + str(damage) + ")")
+
+    def aian(self):
+        damage = self.power * self.waza * 3
+        print(self.name + "がアイアンテールをした。(ダメージ：" + str(damage) + ")")
+    
     
 class Raichu(Pikachu):
-    def __init__(self, power, attack, defense, speed, technique1, technique2, technique3, technique4, technique5, technique6):
-        super().__init__(power, attack, defense, speed, technique1, technique2, technique3)
-        self.technique4 = technique4
-        self.technique5 = technique5
-        self.technique6 = technique6
+    def __init__(self, name, power, attack, defense, speed):
+        super().__init__(name, power, attack, defense, speed)
+        self.waza = 2
 
-pikachu = Pikachu(100, 200, 300, 400, "電光石火", "10万ボルト", "アイアンテール")
-raichu = Raichu(100, 200, 300, 400, "電光石火", "10万ボルト", "アイアンテール", "ロケット頭突き", "かみなりパンチ", "かみなり")
+    def rocket(self):
+        damage = self.power * self.waza * 3
+        print(self.name + "がロケット頭突きをした。(ダメージ：" + str(damage) + ")")
 
-print(raichu.status())
+    def pikapanch(self):
+        damage = self.power * self.waza * 3
+        print(self.name + "がかみなりパンチをした。(ダメージ：" + str(damage) + ")")
+
+    def pika(self):
+        damage = self.power * self.waza * 3
+        print(self.name + "がかみなりをした。(ダメージ：" + str(damage) + ")")
+
+pikachu = Pikachu("さとしピカチュウ", 100, 200, 300, 400)
+raichu = Raichu("さとしライチュウ", 100, 200, 300, 400)
+
+pikachu.denkou()
+pikachu.bolt()
+pikachu.aian()
+
+raichu.denkou()
+raichu.pikapanch()
+raichu.pika()
