0992c5134d027178168a499b1c1e1694d096de68
inagaki <hdj16802@gmail.com>
2021-05-01 19:22:40 +0900

    k210アップしなおし

--- k210_01.py
+++ k210_01.py
@@ -1,6 +1,48 @@
-import Bible
 import os
 import re
+import collections
 
-input_word = input("探したい単語を入力")
-text = Bible.read(input_word)
+
+def isInteger(value):
+    return re.match(r"^\d+$", value) is not None
+
+def isAlpha(value):
+    """
+    半角英字チェック
+    :param value: チェック対象の文字列
+    :rtype: チェック対象文字列が、全て半角英字の場合 True
+    """
+    return re.match(r"^[a-zA-Z]+$", value) is not None
+
+def read(value):
+    file = os.path.abspath("bible.txt")
+    f = open(file, 'r')
+    data = f.read()
+    f.close
+    list_word = re.split(r'[^a-zA-Z]+', data)
+    return list_word
+
+value = input("探したい英単語を入力")
+if isAlpha(value)== True:
+    list_word = read(value)
+    cnt = list_word.count(value)
+    
+    #対象単語があった場合
+    if cnt != 0:
+        print(str(value) + "は" + str(cnt) + "回")
+
+        rank = input("単語出現ランキングの表示順位を入力:")
+        if isInteger(rank) == True:
+            c = collections.Counter(list_word)
+            result = c.most_common(int(rank))
+            k = 1
+            for i in result:
+                jyuni = str(k) + "位"
+                print(str(k) + "位" + (i[0]) + ":"+ str((i[-1])) + "回" )
+                k += 1
+
+        else:
+            print("ランキングは半角数字で入力してください。")
+    #対象単語がなかった場合
+    else:
+        print("その単語はありません。")
