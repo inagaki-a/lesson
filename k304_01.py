0f2ed924912f7d9b13a1bf8e5b53faf9e66b4fbc
inagaki <hdj16802@gmail.com>
2021-05-22 16:45:07 +0900

    304_01オーバーライドしたつもりです（笑）

--- k304_01.py
+++ k304_01.py
@@ -1,20 +1,26 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
+class Fish:
+    def __init__(self):
+        self.name = "魚"
+        self.swim = ""
 
-class Fish(Animal):
-    def __init__(self, name, swim):
-        super().__init__(name)
-        self.swim = swim
+class Demekin(Fish):
+    def __init__(self):
+        super().__init__()
+        self.name = "出目金"
+        self.swim = "ふりふり"
 
-    def action(self):
-        return self.name + "は" + self.swim + "泳ぐ。"
+class Shake(Fish):
+    def __init__(self):
+        super().__init__()
+        self.name = "サメ"
+        self.swim = "すいすい"
 
+def action(self):
+    return self.name + "は" + self.swim + "泳ぐ。"
 
-fish1 = Fish('出目金', 'ふりふり')
-fish2 = Fish('サメ', 'すいすい')
-fish3 = Fish('シーラカンス', 'ゆらゆら')
+sakana = Fish()
+demekin = Demekin()
 
-print(fish1.action())
-print(fish2.action())
-print(fish3.action())
+print(action(sakana))
+print(action(demekin))
+print(action(sakana))
