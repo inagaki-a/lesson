30b058f695126c8ee515b9eb60c1819c522450aa
inagaki <hdj16802@gmail.com>
2021-05-22 16:44:09 +0900

    301_04人が犬に名前を付けました

--- k301_04.py
+++ k301_04.py
@@ -1,43 +1,61 @@
 class Human:
 
-    #初期化メソッド
-    def __init__(self, name, weight, Pet): #初期化
+    def __init__(self, name, weight, pet):
+        """初期化メソッド"""
         self.__name = name 
         self.weight = weight 
-        self.pet = Pet
+        self.pet = pet
     
-    #名前を取得する
     def get_name(self):
+        """名前を取得する"""
         return self.name
 
-    #体重を取得する
     def get_weight(self):
+        """体重を取得する"""
         return self.weight
 
     def info(self):
+        """情報を表示する"""
         return '飼い主：' + self.__name + self.weight + '㎏　ペット：' + self.pet.name + self.pet.weight
+
+    def pet_naming(self, value):
+        #ペットの名前を付ける
+        value = value
+        self.pet.pet_naming(value)
 
 class Pet:
 
-    #初期化メソッド
     def __init__(self, name, weight):
+        """初期化メソッド """
         self.name = name
         self.weight = weight
 
-    #名前を取得する
     def pet_get_name(self):
+        """名前を取得する """
         return self.pet
-    
-    #体重を取得する
+
+    def pet_naming(self, name):
+        """ペットの名前を付けられる"""
+        self.name = name
+
     def pet_get_weight(self):
+        """体重を取得する """
         return self.weight
     
-
+"""
 pet1 = Pet('たろたろ', '10.2')
 human1 = Human('たろ', '100.3', pet1)
 
 pet2 = Pet('じろじろ', '30.2')
 human2 = Human('じろじろ', '70.5', pet2)
+"""
 
-print(human1.info())
-print(human2.info())
+tarotaro = Pet("たろたろ", '10.2')
+taro = Human('たろ', '100.3', tarotaro)
+taro.pet_naming('しば')
+
+jirojiro = Pet('じろじろ', '30.2')
+jiro = Human('じろ', '70.5', jirojiro)
+
+print(taro.info())
+print(jiro.info())
